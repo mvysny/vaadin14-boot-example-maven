fab1e6f372ef6f55f875519ec8d923aef48b52a2
Martin Vysny <mavi@vaadin.com>
2019-12-09 15:04:45 +0200

    Added example on how to style the app

--- src/main/java/com/vaadin/starter/skeleton/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/MainView.java
@@ -1,6 +1,7 @@
 package com.vaadin.starter.skeleton;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -11,6 +12,7 @@ import com.vaadin.flow.server.PWA;
  */
 @Route("")
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
+@CssImport("./styles/app.css")
 public class MainView extends VerticalLayout {
 
     public MainView() {
