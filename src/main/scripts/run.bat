bfa7e166ebfbda5f94ae4836c6441d3039ca1406
Martin Vysny <mavi@vaadin.com>
2021-03-24 11:57:18 +0200

    Renamed ManualJetty to Main

--- src/main/scripts/run.bat
+++ src/main/scripts/run.bat
@@ -7,7 +7,7 @@ rem Add platform runtime libraries
 for %%i in (libs\*.jar) do call :cpappend %%i
 
 rem Launch the runtime
-java -classpath "%CLSPATH%" com.vaadin.starter.skeleton.ManualJetty %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%CLSPATH%" com.vaadin.starter.skeleton.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 if "%OS%"=="Windows_NT" @endlocal
 
